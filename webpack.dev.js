1a9dcc8da3e74f7debcc99a27b4cf5bbb5d48242
Yahmice <83534639+Yahmice@users.noreply.github.com>
2024-12-01 16:08:43 +0500

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const webpack = require("webpack");
 const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const common = require("./webpack.common.config.js");
 
 module.exports = merge(common, {
   // Set the mode to development or production
